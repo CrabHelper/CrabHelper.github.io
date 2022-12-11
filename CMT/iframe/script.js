26bbe1d19b088e902e0e966e502388b6f4784e99
CrabHelper <91166765+CrabHelper@users.noreply.github.com>
2022-12-11 18:37:59 -0500

    Delete script.js

--- CMT/iframe/script.js
+++
@@ -1,3 +0,0 @@
-window.addEventListener("load", () => {
-    document.getElementById('yeet').innerHTML = `<iframe src="https://crazyalgebra.com/" height="${window.innerHeight}" width="${window.innerWidth}" frameborder="0"></iframe>`
-});
