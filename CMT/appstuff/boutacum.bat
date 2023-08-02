c5074b673db113d2eec86e4795224c03fd413f87
CrabHelper <91166765+CrabHelper@users.noreply.github.com>
2023-08-02 11:23:24 -0500

    Add files via upload

---
+++ CMT/appstuff/boutacum.bat
@@ -0,0 +1,10 @@
+@echo off
+set "file=track12.mp3"
+( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
+  echo Sound.URL = "%userprofile%\desktop\customfiles\Im bout to cum with choir.mp3"
+  echo Sound.Controls.play
+  echo do while Sound.currentmedia.duration = 0
+  echo wscript.sleep 100
+  echo loop
+  echo wscript.sleep (int(Sound.currentmedia.duration^)+1^)*1000) >sound.vbs
+start /min sound.vbs
